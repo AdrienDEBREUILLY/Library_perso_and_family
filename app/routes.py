bdcc3ef856204467f7e41d5c40233e7b11c9720e
AdrienDEBREUILLY <adrien.debreuilly@gmail.com>
2023-04-21 15:24:18 +0200

    final version 1 before admin account system

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, url_for, flash, redirect, request,
 from flask_login import login_user, current_user, logout_user, login_required
 from app import db, bcrypt
 from app.forms import LoginForm, RegistrationForm, AddBookForm, UpdateBookForm
-from app.models import User, Book
+from app.models import User, Book, BookListBook, BookSerie, BorrowedBook
 
 routes = Blueprint('routes', __name__)
 
@@ -59,14 +59,14 @@ def index():
     return render_template('index.html', books=books)
 
 
-@routes.route('/livre_liste')
+@routes.route('/book_list')
 @login_required
 def book_list():
     books = Book.query.all()
     return render_template('book_list.html', books=books)
 
 
-@routes.route('/livre_detail/<int:book_id>')
+@routes.route('/book_detail/<int:book_id>')
 @login_required
 def book_detail(book_id):
     book = Book.query.get_or_404(book_id)
@@ -74,44 +74,57 @@ def book_detail(book_id):
 
 
 # Add, update, and delete book routes from previous response
-@routes.route('/livre_ajouter', methods=['GET', 'POST'])
+@routes.route('/book_add', methods=['GET', 'POST'])
 @login_required
 def book_add():
     form = AddBookForm()
     if form.validate_on_submit():
-        book = Book(title=form.title.data, author=form.author.data, user_id=current_user.id_user)
+        book = Book(title=form.title.data, author=form.author.data)
         db.session.add(book)
         db.session.commit()
         flash('Votre livre a été ajouté avec succès!', 'success')
         return redirect(url_for('routes.index'))
-    return render_template('livre_ajouter.html', title='Ajouter un livre', form=form)
+    return render_template('book_add.html', title='Ajouter un livre', form=form)
 
 
-@routes.route('/livre_modifier/<int:book_id>', methods=['GET', 'POST'])
+@routes.route('/book_update/<int:book_id>', methods=['GET', 'POST'])
 @login_required
 def book_update(book_id):
     book = Book.query.get_or_404(book_id)
-    if book.user_id != current_user.id_user:
-        abort(403)
+    # if book.user_id != current_user.id_user:
+    #     abort(403)
     form = UpdateBookForm()
     if form.validate_on_submit():
         book.title = form.title.data
         book.author = form.author.data
         db.session.commit()
         flash('Votre livre a été mis à jour avec succès!', 'success')
-        return redirect(url_for('routes.livre_detail', book_id=book.id_book))
+        return redirect(url_for('routes.book_detail', book_id=book.id_book))
     elif request.method == 'GET':
         form.title.data = book.title
         form.author.data = book.author
-    return render_template('livre_modifier.html', title='Modifier un livre', form=form)
+    return render_template('book_update.html', title='Modifier un livre', form=form, book=book)
 
 
-@routes.route('/livre_supprimer/<int:book_id>', methods=['POST'])
+@routes.route('/book_delete/<int:book_id>', methods=['POST'])
 @login_required
 def book_delete(book_id):
     book = Book.query.get_or_404(book_id)
-    if book.user_id != current_user.id_user:
-        abort(403)
+    # if book.user_id != current_user.id_user:
+    #     abort(403)
+    # Supprimer les entrées associées dans la table BookListBook
+    book_list_books = BookListBook.query.filter_by(book_id=book.id_book).all()
+    for book_list_book in book_list_books:
+        db.session.delete(book_list_book)
+    # Supprimer les entrées associées dans la table BookSerie
+    book_series = BookSerie.query.filter_by(book_id=book.id_book).all()
+    for book_serie in book_series:
+        db.session.delete(book_serie)
+    # Supprimer les entrées associées dans la table BorrowedBook
+    borrowed_books = BorrowedBook.query.filter_by(book_id=book.id_book).all()
+    for borrowed_book in borrowed_books:
+        db.session.delete(borrowed_book)
+    # Supprimer le livre
     db.session.delete(book)
     db.session.commit()
     flash('Votre livre a été supprimé avec succès!', 'success')
